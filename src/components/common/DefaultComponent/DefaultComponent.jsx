81546ec3375d3eb80f8d29bd1f36b5e956dd7aac
PHUONGNAM101203 <pnamhuynhle@gmail.com>
2025-03-14 20:38:15 +0700

    create FooterComponent Folder

--- src/components/common/DefaultComponent/DefaultComponent.jsx
+++ src/components/common/DefaultComponent/DefaultComponent.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import HeaderComponent from '../../client/HeaderComponent/HeaderComponent'
+import React from 'react';
+import HeaderComponent from '../../client/HeaderComponent/HeaderComponent';
+import FooterComponent from '../../client/FooterComponent/FooterComponent';
+import styled from 'styled-components';
 
-const DefaultComponent = ({children}) => {
+const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const ContentWrapper = styled.main`
+  flex: 1;
+  padding: 20px;
+`;
+
+const DefaultComponent = ({ children }) => {
   return (
-    <>
-        <HeaderComponent/>
-        {children}
-    </>
-  )
-}
+    <LayoutWrapper>
+      <HeaderComponent />
+      <ContentWrapper>{children}</ContentWrapper>
+      <FooterComponent />
+    </LayoutWrapper>
+  );
+};
 
-export default DefaultComponent
+export default DefaultComponent;
